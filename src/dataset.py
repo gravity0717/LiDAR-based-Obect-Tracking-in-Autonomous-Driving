3bf18639288e9b52df9e37ae14b02e6899cfa7bb
jinwon <cjinone2@gmail.com>
2025-01-28 23:27:57 +0900

    kitti implemented

--- src/dataset.py
+++ src/dataset.py
@@ -5,7 +5,8 @@ import pykitti
 
 class Dataset:
     def __init__(self, mat_path):
-        self.datas = io.loadmat(mat_path)
+        if mat_path:
+            self.datas = io.loadmat(mat_path)
         self.cur = 0
     
     def __len__(self):
@@ -44,3 +45,31 @@ class Dataset:
     def wait(self):
         self.cur -= 1
         
+class KITTI(Dataset):
+    def __init__(self):
+        super().__init__()
+        basedir = '/home/poseidon/workspace/dataset/KITTI/raw'
+        date   = "2011_09_26"
+        drive  = "0018"
+        datas = pykitti.raw(basedir, date, drive)
+
+    def __len__(self):
+        return len(list(self.datas.velo))
+
+
+    def __next__(self):
+        if self.cur >= len(self):
+            raise StopIteration
+        
+        x, y, z, reflectance  = self.datas.velo[self.cur] #Note) Reflectance: 반사 강도 
+        image = self.datas.cam2[self.cur]
+
+        xyz = np.vstack([x, y, z])
+        
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(xyz.T)
+        
+        o3d_image = o3d.geometry.Image(image)
+    
+        return pcd, o3d_image
+    
